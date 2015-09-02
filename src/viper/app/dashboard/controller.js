2d2745446cd548d5398de6242861d58e1dcc1cf9
Guido Tarsia <gtarsia@PC16.virtualmind.net>
2015-09-02 15:26:40 -0300

    Add index view for dashboard

---
+++ src/viper/app/dashboard/controller.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('dashboardController', dashboardController);
+
+    controller.$inject = ['$location']; 
+
+    function dashboardController($location) {
+        var vm = this;
+        vm.title = 'controller';
+
+        activate();
+
+        function activate() { }
+    }
+})();

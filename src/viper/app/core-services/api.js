0cc695805e2f3c5bc016242d64ae40e91b781b7e
Guido Tarsia <gtarsia@PC16.virtualmind.net>
2015-09-02 15:59:47 -0300

    Reorganize app services

---
+++ src/viper/app/core-services/api.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    'use strict';
+
+    angular
+    .module('viper')
+    .factory('api', ['$http', 'url', 'token', function ($http, url, token) {
+        function ajax(method, _url) {
+            return function (data) {
+                return $http({
+                    url: url.api.api + _url,
+                    data: data,
+                    method: method,
+                    headers: {
+                        'Authorization': 'Bearer ' + token.val()
+                    }
+                });
+            }
+        }
+        return function (url) {
+            return {
+                get: ajax('get', url),
+                post: ajax('post', url)
+            }
+        }
+    }])
+})();

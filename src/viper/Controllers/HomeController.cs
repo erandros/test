04c5d43816a4a37daa38046374da85402d5cfb04
Guido Tarsia <gtarsia@PC16.virtualmind.net>
2015-09-04 15:14:27 -0300

    Create new HttpPost to login using ASP NET form

--- src/viper/Controllers/HomeController.cs
+++ src/viper/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using Newtonsoft.Json;
+using Microsoft.AspNet.Http;
 
 namespace viper.Controllers
 {
@@ -16,8 +18,24 @@ namespace viper.Controllers
         public IActionResult Login()
         {
             ViewBag.Message = "Login Page";
-
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            var api = new API();
+            var response = api.LoginRequest(Request, username, password).Result;
+            var result = response.Content.ReadAsStringAsync().Result;
+            dynamic data = JsonConvert.DeserializeObject(result);
+            var cookie = "Bearer " + data.access_token.Value;
+            var cookieOptions = new CookieOptions() { HttpOnly = true, Expires = DateTime.Now.AddDays(1) };
+            Response.Cookies.Append("Authorization", cookie, cookieOptions);
+            if (response.StatusCode != System.Net.HttpStatusCode.OK)
+            {
+                return View();
+            }
+            else return RedirectToAction("Index", "Dashboard");
         }
 
         public IActionResult Error()

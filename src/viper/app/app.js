d9704eeb6b2ee1c34847a8a6bbbf76ee3d5ae33a
Guido Tarsia <gtarsia@PC16.virtualmind.net>
2015-09-04 15:15:19 -0300

    Remove redirection from js

--- src/viper/app/app.js
+++ src/viper/app/app.js
@@ -5,8 +5,4 @@
     var viper =
         angular.module('viper', ['smart-table', 'ui.bootstrap']);
 
-    viper.run(['url', 'token', function (url, token) {
-        if (!token.hasVal() && location.pathname != url.login)
-            url.redirect.toLogin();
-    }])
 })();

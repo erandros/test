eb42c3342e49bb7dcebce852ff884e1c9b59f4e7
Guido Tarsia <gtarsia@PC16.virtualmind.net>
2015-10-28 14:46:42 -0300

    Refactorize into different controllers

--- src/viper/Controllers/UsersController.cs
+++ src/viper/Controllers/UsersController.cs
@@ -3,14 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
-using Microsoft.AspNet.Authorization;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace viper.Controllers
 {
-    [Authorize]
-    public class DashboardController : Controller
+    public class UsersController : Controller
     {
         // GET: /<controller>/
         public IActionResult Index()

729829a38aa85276f0b42d4827881ac604fb6a77
Guido Tarsia <gtarsia@virtualmindsoftware.com>
2015-11-03 16:54:06 -0300

    Create vp-table

---
+++ src/viper/app/core-directives/vp-table.js
@@ -0,0 +1,29 @@
+﻿(function () {
+    'use strict';
+
+    angular
+    .module('viper')
+    .directive('vpTable', ['$modal', function ($modal) {
+        function ViperTable($modalInstance, $scope) {
+            var vm = this;
+        }
+        function link(scope, element, attrs) {
+            element.bind('click', function () {
+                $modal.open({
+                    templateUrl: '/modals/edition.html',
+                    controller: ['$modalInstance', '$scope', ViperTable]
+                })
+                .result.then(function (client) {
+                    console.log("Should be adding item");
+                }, function () {
+
+                })
+            })
+        }
+        return {
+            restrict: 'E',
+            link: link,
+            templateUrl: 'templates/vp-table.html'
+        };
+    }])
+})();

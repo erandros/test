ea518a3b12b95dd560c274de98345a26901be1b9
Guido Tarsia <guidotarsia@gmail.com>
2015-11-11 11:56:28 -0300

    Remove wrong code and unneeded controllers

--- src/viper/app/core-controllers/title.js
+++
@@ -1,9 +0,0 @@
-﻿(function () {
-    'use strict';
-
-    angular
-    .module('viper')
-    .controller('titleController', ['$scope', 'application', function ($scope, application) {
-        $scope.Title = application.Title;
-    }])
-})();

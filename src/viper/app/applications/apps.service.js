da19a53182f67e699d5e48415409a0042187513d
Guido Tarsia <guidotarsia@gmail.com>
2015-11-11 13:06:56 -0300

    Finish applications view, add flattening code to vp-table

--- src/viper/app/applications/apps.service.js
+++ src/viper/app/applications/apps.service.js
@@ -4,22 +4,10 @@
     angular
     .module('viper')
     .factory('applications', ['api', function (api) {
-        var res = api('/user/applications');
+        var res = api('/user/users/my/applications');
         var _get = res.get;
         res.get = function (data) {
             return _get(data)
-            .then(function (res) {
-                res.data = res.data.map(function (el) {
-                    return {
-                        Id: el.ApplicationId,
-                        Type: el.ApplicationType,
-                        Name: el.Name,
-                        Title: el.Title,
-                        IsDefault: el.IsDefault
-                    }
-                })
-                return res;
-            })
         }
         return res;
     }])

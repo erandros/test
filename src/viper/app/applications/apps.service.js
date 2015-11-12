8d32a2e3429bb050a387267e1eb77a49853a3cd8
Guido Tarsia <gtarsia@virtualmindsoftware.com>
2015-11-12 18:36:45 -0300

    Update routes

--- src/viper/app/applications/apps.service.js
+++ src/viper/app/applications/apps.service.js
@@ -4,7 +4,7 @@
     angular
     .module('viper')
     .factory('applications', ['api', function (api) {
-        var res = api('/user/users/my/applications');
+        var res = api('/applications');
         var _get = res.get;
         res.get = function (data) {
             return _get(data)

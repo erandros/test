c3bca1accfc0571b6cddcf12f67f0c765802db2f
Guido Tarsia <gtarsia@virtualmindsoftware.com>
2015-11-17 11:59:24 -0300

    Use camelSplit for the modal fields

--- src/viper/app/prototypes/string.js
+++ src/viper/app/prototypes/string.js
@@ -8,4 +8,10 @@
     String.prototype.pluralize = function () {
         return this + 's';
     }
+
+    String.prototype.camelSplit = function () {
+        return this
+        .replace(/([A-Z])/g, ' $1')
+        .replace(/^./, function (str) { return str.toUpperCase(); })
+    }
 })();

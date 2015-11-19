50ac29a22fd68eda94da98ddb4fbfe8b1b644923
Guido Tarsia <gtarsia@virtualmindsoftware.com>
2015-11-19 17:40:19 -0300

    Refactor API service further

--- src/viper/app/core-services/api.js
+++ src/viper/app/core-services/api.js
@@ -5,10 +5,19 @@
     .module('viper')
     .factory('api', ['$http', function ($http) {
         function ajax(method, _url) {
+            _url = '/api' + _url;
             return function (data) {
+                if (method == 'put' || method == 'delete' || method == 'post')
+                    if (!data) throw new Error('Required data is not truthy');
+                if (method == 'put' || method == 'delete') {
+                    if (!data.Id) throw new Error('Required Id field on data is not truthy');
+                    _url += '/' + data.Id;
+                }
+                if (method == 'delete')
+                    data = null; //Don't send any data on delete
                 return $http({
                     method: method,
-                    url: '/api' + _url,
+                    url: _url,
                     data: data
                 });
             }
@@ -18,7 +27,9 @@
             create: function (url) {
                 return {
                     get: ajax('get', url),
-                    post: ajax('post', url)
+                    post: ajax('post', url),
+                    delete: ajax('delete', url),
+                    put: ajax('put', url)
                 }
             }
         }

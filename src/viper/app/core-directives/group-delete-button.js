59419eb5dd7fea37abcfe96b96b1d50fbf988a2c
Guido Tarsia <gtarsia@virtualmindsoftware.com>
2015-11-26 12:24:34 -0300

    Add delete button

---
+++ src/viper/app/core-directives/group-delete-button.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    'use strict'
+
+    angular
+    .module('viper')
+    .directive('groupDeleteButton', function () {
+        
+        function link(scope, element, attrs) {
+            element.bind('click', function () {
+
+            });
+        }
+        return {
+            link: link,
+            template: '<button class="btn btn-danger" ng-disabled="true">Delete</button>',
+        }
+    });
+})();

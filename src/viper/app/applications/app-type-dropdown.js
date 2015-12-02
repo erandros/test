dfe341a46f6525af2704b5b880459691c3092d3d
Guido Tarsia <guidotarsia@gmail.com>
2015-12-02 18:57:43 -0300

    Add app-type-dropdown

---
+++ src/viper/app/applications/app-type-dropdown.js
@@ -0,0 +1,27 @@
+﻿(function () {
+    'use strict'
+
+    angular
+    .module('viper')
+    .directive('appTypeDropdown', function () {
+        function link(scope, element, attrs, ctrl) {
+            element.bind('click', function () {
+            });
+        }
+        return {
+            link: link,
+            template: '<select class="form-control" ng-model="selectedAppType"' + 
+                      ' ng-options="type as type.Name for type in appTypes track by type.Id">' +
+                      ' </select><button class="btn btn-primary">Change</button>',
+            controller: ['$scope', 'appTypes', AppTypeDropdown]
+        }
+        function AppTypeDropdown($scope, appTypes) {
+            appTypes.get()
+            .then(function (res) {
+                $scope.appTypes = res.data;
+                $scope.appTypes.unshift({ Id: -1, Name: '' })
+                $scope.selectedAppType = $scope.appTypes[0];
+            })
+        }
+    });
+})();
---
+++ src/viper/app/applications/app-types.service.js
@@ -0,0 +1,10 @@
+﻿(function () {
+    'use strict';
+
+    angular
+    .module('viper')
+    .factory('appTypes', ['api', function (api) {
+        var appTypesApi = api.create('/applicationtypes');
+        return appTypesApi;
+    }])
+})();

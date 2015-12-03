787633c074712e63361b550c8c99a5705c4b39c0
Guido Tarsia <gtarsia@virtualmindsoftware.com>
2015-12-03 17:24:03 -0300

    Move appTypes as getTypes of applications method

--- src/viper/app/applications/apps.service.js
+++ src/viper/app/applications/apps.service.js
@@ -5,6 +5,12 @@
     .module('viper')
     .factory('applications', ['api', function (api) {
         var appsApi = api.create('/applications');
+        appsApi.getTypes = function () {
+            return api.request({
+                method: method,
+                url: '/api/applicationtypes'
+            });
+        }
         return appsApi;
     }])
 })();

f8807d11feb904f3f6da7400401cdfde94c1124b
Guido Tarsia <guidotarsia@gmail.com>
2015-12-14 16:49:49 -0300

    Ability to save form

--- src/viper/app/core-directives/vp-form.js
+++ src/viper/app/core-directives/vp-form.js
@@ -3,7 +3,8 @@
 
     angular
     .module('viper')
-    .directive('vpForm', ['$modal', '$injector', '$location', 'utils', function ($modal, $injector, $location, utils) {
+    .directive('vpForm', ['$modal', '$injector', '$location', 'utils', 'notify',
+        function ($modal, $injector, $location, utils, notify) {
         function link(scope, element, attrs, ctrl) {
             var type = attrs['type'];
             scope.fields = utils.typify(attrs["fields"]);
@@ -17,6 +18,12 @@
             ctrl.fill();
             scope.type = attrs["type"];
             scope.title = attrs["title"];
+            scope.save = function (form) {
+                scope.api.put(form)
+                .then(function (res) {
+                    notify.success("Saved");
+                })
+            }
         }
         return {
             restrict: 'E',

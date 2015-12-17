973284c649aa059127539875d622a251060990b1
Guido Tarsia <gtarsia@virtualmindsoftware.com>
2015-12-17 17:02:38 -0300

    Refactor checkbox-list code

--- src/viper/app/core-directives/vp-checkbox-list.js
+++ src/viper/app/core-directives/vp-checkbox-list.js
@@ -1,5 +1,4 @@
-﻿var _scope;
-(function () {
+﻿(function () {
 
     angular
     .module('viper')
@@ -8,17 +7,23 @@
             require: '^vpForm',
             templateUrl: '/templates/vp-checkbox-list.html',
             link: function (scope, element, attr, ctrl) {
-                _scope = scope;
                 scope.api = $injector.get(scope.field.params[0]);
                 $parse;
-                var _comparator = scope[scope.field.params[1]];
+                var _comparator = scope.field.params[1];
+                var _comparatorFn = scope[_comparator];
                 scope.comparator = function (obj1, obj2) {
-                    if (angular.isFunction(_comparator)) return _comparator(obj1, obj2);
+                    if (angular.isFunction(_comparatorFn)) return _comparatorFn(obj1, obj2);
                     else return obj1[_comparator] == obj2[_comparator];
+                }
+                var _map = scope.field.params[2];
+                var _mapFn = scope[_map];
+                scope.map = function (obj) {
+                    if (angular.isFunction(_mapFn)) return _mapFn(obj);
+                    else return obj[_map];
                 }
                 scope.api.getAll()
                 .then(function (res) {
-                    scope.checkboxes = res.data;
+                    scope.items = res.data;
                 })
             }
         };
